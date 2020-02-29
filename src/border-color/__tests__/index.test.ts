23f9fed7232f619b8f7c84dba2dd021649c66fd3
Hugo <hugonteifeh@gmail.com>
2020-02-29 14:33:46 +0100

    Add borderBottomColor and borderLeftColor

--- src/border-color/__tests__/index.test.ts
+++ src/border-color/__tests__/index.test.ts
@@ -1,4 +1,9 @@
-import { borderTopColor, borderRightColor } from '../index'
+import {
+  borderTopColor,
+  borderRightColor,
+  borderBottomColor,
+  borderLeftColor
+} from '../index'
 import { rgb } from '../../color/internal'
 
 test('borderTopColor(RGB)', () => {
@@ -10,5 +15,17 @@ test('borderTopColor(RGB)', () => {
 test('borderRightColor(RGB)', () => {
   const received = borderRightColor(rgb([255, 255, 255]))
   const expected = { borderRightColor: 'rgb(255, 255, 255)' }
+  expect(received).toEqual(expected)
+})
+
+test('borderBottomColor(RGB)', () => {
+  const received = borderBottomColor(rgb([255, 255, 255]))
+  const expected = { borderBottomColor: 'rgb(255, 255, 255)' }
+  expect(received).toEqual(expected)
+})
+
+test('borderLeftColor(RGB)', () => {
+  const received = borderLeftColor(rgb([255, 255, 255]))
+  const expected = { borderLeftColor: 'rgb(255, 255, 255)' }
   expect(received).toEqual(expected)
 })
--- src/border-color/index.ts
+++ src/border-color/index.ts
@@ -24,3 +24,27 @@ export const borderRightColor = (
 ): { borderRightColor: string } => ({
   borderRightColor: isGlobalCssKeyword(value) ? value : serializeColor(value)
 })
+
+/**
+ * Creates a declaration object for the **`border-bottom-color`** property.
+ * @category Declaration function
+ * @formalSyntax <color>
+ * @implentationReference https://www.w3.org/TR/css-backgrounds-3/#the-border-color
+ */
+export const borderBottomColor = (
+  value: Color | GlobalCssKeyword
+): { borderBottomColor: string } => ({
+  borderBottomColor: isGlobalCssKeyword(value) ? value : serializeColor(value)
+})
+
+/**
+ * Creates a declaration object for the **`border-left-color`** property.
+ * @category Declaration function
+ * @formalSyntax <color>
+ * @implentationReference https://www.w3.org/TR/css-backgrounds-3/#the-border-color
+ */
+export const borderLeftColor = (
+  value: Color | GlobalCssKeyword
+): { borderLeftColor: string } => ({
+  borderLeftColor: isGlobalCssKeyword(value) ? value : serializeColor(value)
+})
