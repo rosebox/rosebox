16ac4e1e593b39d2f9436ee2aee5440fa458eae6
Hugo <hugonteifeh@gmail.com>
2020-02-29 18:49:42 +0100

    Add borderBottomStyle

--- src/border-style/__tests__/index.test.ts
+++ src/border-style/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { borderTopStyle, borderRightStyle } from '../index'
+import { borderTopStyle, borderRightStyle, borderBottomStyle } from '../index'
 
 test('borderTopStyle(hidden)', () => {
   const received = borderTopStyle('hidden')
@@ -9,5 +9,11 @@ test('borderTopStyle(hidden)', () => {
 test('borderRightStyle(hidden)', () => {
   const received = borderRightStyle('hidden')
   const expected = { borderRightStyle: 'hidden' }
+  expect(received).toEqual(expected)
+})
+
+test('borderBottomStyle(hidden)', () => {
+  const received = borderBottomStyle('hidden')
+  const expected = { borderBottomStyle: 'hidden' }
   expect(received).toEqual(expected)
 })
--- src/border-style/index.ts
+++ src/border-style/index.ts
@@ -23,3 +23,15 @@ export const borderRightStyle = (
 ): { borderRightStyle: string } => ({
   borderRightStyle: value
 })
+
+/**
+ * Creates a declaration object for the **`border-bottom-style`** property.
+ * @category Declaration function
+ * @formalSyntax <line-style>
+ * @implentationReference https://www.w3.org/TR/css-backgrounds-3/#the-border-style
+ */
+export const borderBottomStyle = (
+  value: LineStyle | GlobalCssKeyword
+): { borderBottomStyle: string } => ({
+  borderBottomStyle: value
+})
