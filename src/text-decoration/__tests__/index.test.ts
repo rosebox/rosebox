75d8847dd07854cd2b706b9e1c2fec0d3d92c889
Hugo <hugonteifeh@gmail.com>
2020-03-01 17:32:27 +0100

    Fix typo

--- src/text-decoration/__tests__/index.test.ts
+++ src/text-decoration/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import { textDecorationLine } from '../index'
 
-test('textDecorationLine(line-through blink overline underline')', () => {
+test('textDecorationLine(line-through blink overline underline)', () => {
   const received = textDecorationLine('line-through blink overline underline')
   const expected = {
     textDecorationLine: 'line-through blink overline underline'
