bcaea61e97c8bd0f6be1bf2c4aaa287702cf5457
Hugo <hugonteifeh@gmail.com>
2020-03-01 12:36:01 +0100

    Fix docs - implementationReferene for borderStyle

--- src/border-style/index.ts
+++ src/border-style/index.ts
@@ -69,7 +69,7 @@ const serializeBorderStyle = (value: BorderStyle): string =>
  * Creates a declaration object for the **`border-width`** property.
  * @category Declaration function
  * @formalSyntax <line-style>{1,4}
- * @implentationReference https://www.w3.org/TR/css-backgrounds-3/#the-border-width
+ * @implentationReference https://www.w3.org/TR/css-backgrounds-3/#the-border-style
  */
 export const borderStyle = (
   value: BorderStyle | GlobalCssKeyword
