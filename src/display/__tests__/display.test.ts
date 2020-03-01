ae7992dfb53558e9cafd5bc52fc321af753f6941
Hugo <hugonteifeh@gmail.com>
2020-03-01 13:09:29 +0100

    Add extra test-case to display

--- src/display/__tests__/display.test.ts
+++ src/display/__tests__/display.test.ts
@@ -5,3 +5,9 @@ test('display()', () => {
   const expected = { display: 'list-item' }
   expect(received).toEqual(expected)
 })
+
+test('display()', () => {
+  const received = display('inline flow')
+  const expected = { display: 'inline flow' }
+  expect(received).toEqual(expected)
+})
