9261e245630aa236edb12b867cb9b62d285ad0be
Hugo <hugonteifeh@gmail.com>
2020-03-01 12:46:54 +0100

    Add overflow

--- src/overflow/__tests__/index.test.ts
+++ src/overflow/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { overflowX, overflowY } from '../index'
+import { overflowX, overflowY, overflow } from '../index'
 
 test('overflowX(auto)', () => {
   const received = overflowX('auto')
@@ -12,6 +12,22 @@ test('overflowY(auto)', () => {
   const received = overflowY('auto')
   const expected = {
     overflowY: 'auto'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('overflow(auto)', () => {
+  const received = overflow(['auto', 'hidden'])
+  const expected = {
+    overflow: 'auto hidden'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('overflow(auto)', () => {
+  const received = overflow(['auto'])
+  const expected = {
+    overflow: 'auto'
   }
   expect(received).toEqual(expected)
 })
--- src/overflow/index.ts
+++ src/overflow/index.ts
@@ -1,4 +1,6 @@
-import { GlobalCssKeyword } from '../shared/types'
+import { GlobalCssKeyword, isGlobalCssKeyword } from '../shared/types'
+
+type OverflowKeyword = 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto'
 
 /**
  * Creates a declaration object for the **`overflow-x`** property.
@@ -7,7 +9,7 @@ import { GlobalCssKeyword } from '../shared/types'
  * @implementationReference https://www.w3.org/TR/css-overflow-3/#overflow-properties
  */
 export const overflowX = (
-  value: 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto' | GlobalCssKeyword
+  value: OverflowKeyword | GlobalCssKeyword
 ): { overflowX: string } => ({
   overflowX: value
 })
@@ -19,7 +21,30 @@ export const overflowX = (
  * @implementationReference https://www.w3.org/TR/css-overflow-3/#overflow-properties
  */
 export const overflowY = (
-  value: 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto' | GlobalCssKeyword
+  value: OverflowKeyword | GlobalCssKeyword
 ): { overflowY: string } => ({
   overflowY: value
+})
+
+type Overflow =
+  | OverflowKeyword
+  | [OverflowKeyword]
+  | [OverflowKeyword, OverflowKeyword]
+
+const serializeOverFlow = (value: Overflow): string =>
+  !Array.isArray(value)
+    ? value
+    : (value as OverflowKeyword[])
+        .reduce((acc: any, item) => acc + ' ' + item, '')
+        .trim()
+/**
+ * Creates a declaration object for the **`overflow`** property.
+ * @category Declaration function
+ * @formalSyntax [ visible | hidden | clip | scroll | auto ]{1,2}
+ * @implementationReference https://www.w3.org/TR/css-overflow-3/#overflow-properties
+ */
+export const overflow = (
+  value: Overflow | GlobalCssKeyword
+): { overflow: string } => ({
+  overflow: isGlobalCssKeyword(value) ? value : serializeOverFlow(value)
 })
