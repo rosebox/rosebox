4ce6e50710d07f8c2130b693fee5b9b04b592e73
Hugo <hugonteifeh@gmail.com>
2020-03-01 12:32:54 +0100

    Add overflowY

--- src/overflow/__tests__/index.test.ts
+++ src/overflow/__tests__/index.test.ts
@@ -1,9 +1,17 @@
-import { overflowX } from '../index'
+import { overflowX, overflowY } from '../index'
 
 test('overflowX(auto)', () => {
   const received = overflowX('auto')
   const expected = {
     overflowX: 'auto'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('overflowY(auto)', () => {
+  const received = overflowY('auto')
+  const expected = {
+    overflowY: 'auto'
   }
   expect(received).toEqual(expected)
 })
--- src/overflow/index.ts
+++ src/overflow/index.ts
@@ -11,3 +11,15 @@ export const overflowX = (
 ): { overflowX: string } => ({
   overflowX: value
 })
+
+/**
+ * Creates a declaration object for the **`overflow-y`** property.
+ * @category Declaration function
+ * @formalSyntax visible | hidden | clip | scroll | auto
+ * @implementationReference https://www.w3.org/TR/css-overflow-3/#overflow-properties
+ */
+export const overflowY = (
+  value: 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto' | GlobalCssKeyword
+): { overflowY: string } => ({
+  overflowY: value
+})
