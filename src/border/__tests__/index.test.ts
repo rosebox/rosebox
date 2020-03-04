7fc6ef06f3a8e208da32c35611253daa8a0e56b7
Hugo <hugonteifeh@gmail.com>
2020-03-04 21:43:31 +0100

    Add borderLeft

---
+++ src/border/__tests__/index.test.ts
@@ -0,0 +1,35 @@
+import { borderLeft } from '../index'
+import { px } from '../../shared/value-constructors'
+import { rgb } from '../../color/internal'
+
+test('borderLeft([Length<px>])', () => {
+  const received = borderLeft([px(3)])
+  const expected = {
+    borderLeft: '3px'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('borderLeft([Length<px>, LineStyle])', () => {
+  const received = borderLeft([px(3), 'solid'])
+  const expected = {
+    borderLeft: '3px solid'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('borderLeft([Length<px>, LineStyle, Color<RGB>])', () => {
+  const received = borderLeft([px(3), 'solid', rgb([255, 255, 255])])
+  const expected = {
+    borderLeft: '3px solid rgb(255, 255, 255)'
+  }
+  expect(received).toEqual(expected)
+})
+
+test('borderLeft([Length<px>, Color<RGB>])', () => {
+  const received = borderLeft([px(3), rgb([255, 255, 255])])
+  const expected = {
+    borderLeft: '3px rgb(255, 255, 255)'
+  }
+  expect(received).toEqual(expected)
+})
---
+++ src/border/index.ts
@@ -0,0 +1,57 @@
+import {
+  LineWidth,
+  LineStyle,
+  isLengthType,
+  GlobalCssKeyword,
+  isGlobalCssKeyword
+} from '../shared/types'
+import { Color, isColor, serializeColor } from '../color/internal'
+import { serializeLength } from '../shared/serializers'
+
+type AtomicValue = Color | LineStyle | LineWidth
+
+type SingleValue = AtomicValue | [Color] | [LineStyle] | [LineWidth]
+
+type TwoValues =
+  | [Color, LineStyle]
+  | [LineStyle, Color]
+  | [Color, LineWidth]
+  | [LineWidth, Color]
+  | [LineStyle, LineWidth]
+  | [LineWidth, LineStyle]
+
+type ThreeValues =
+  | [Color, LineStyle, LineWidth]
+  | [Color, LineWidth, LineStyle]
+  | [LineStyle, Color, LineWidth]
+  | [LineStyle, LineWidth, Color]
+  | [LineWidth, Color, LineStyle]
+  | [LineWidth, LineStyle, Color]
+
+type BorderSide = SingleValue | TwoValues | ThreeValues
+
+const serializeAtomicValue = (value: AtomicValue) =>
+  isColor(value)
+    ? serializeColor(value)
+    : isLengthType(value)
+    ? serializeLength(value)
+    : value
+
+const serializeBorderSide = (value: BorderSide) =>
+  !Array.isArray(value)
+    ? serializeAtomicValue(value)
+    : (value as AtomicValue[])
+        .reduce((acc: any, item) => acc + ' ' + serializeAtomicValue(item), '')
+        .trim()
+
+/**
+ * Creates a declaration object for the **`border-left`** property.
+ * @category Declaration function
+ * @formalSyntax [ <border-width> || <border-style> || <'border-top-color'> ] | inherit
+ * @implentationReference https://www.w3.org/TR/CSS2/box.html#border-shorthand-properties
+ */
+export const borderLeft = (
+  value: BorderSide | GlobalCssKeyword
+): { borderLeft: string } => ({
+  borderLeft: isGlobalCssKeyword(value) ? value : serializeBorderSide(value)
+})
