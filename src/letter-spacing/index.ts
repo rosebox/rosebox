9a39c42bc1f233374242abcb2801b1880abcc35a
Hugo <hugonteifeh@gmail.com>
2020-03-07 12:13:39 +0100

    Update links for implementation references for letterSpacing

--- src/letter-spacing/index.ts
+++ src/letter-spacing/index.ts
@@ -7,7 +7,7 @@ import { serializeLength } from '../shared/serializers'
  * @category Declaration function
  * @formalSyntax normal | <length>
  * @added 0.1.5
- * @implentationReference https://www.w3.org/TR/css-text-3/#letter-spacing-property
+ * @implentationReference https://www.w3.org/TR/2019/WD-css-text-3-20191113/#letter-spacing-property
  */
 export const letterSpacing = (
   value: 'normal' | Length | GlobalCssKeyword
