45b980a07ddd77766f42b8cf31355765e17f76d8
Hugo <hugonteifeh@gmail.com>
2020-04-12 14:54:38 +0200

    Add docs for the "visibility" property

--- src/core/visibility/index.ts
+++ src/core/visibility/index.ts
@@ -3,7 +3,7 @@ import { GlobalCssKeyword } from '../shared/types'
 /**
  * Creates a declaration object for the **`visibility`** shorthand property.
  * @category Declaration function
- * @formalSyntax visible | hidden | collapse | inherit
+ * @formalSyntaxForValue visible | hidden | collapse | inherit
  * @added 0.1.5
  * @implentationReference https://www.w3.org/TR/2011/REC-CSS2-20110607/visufx.html#propdef-visibility
  */
@@ -15,6 +15,13 @@ export const visibility = (
 })
 
 export type VisibilityDeclration = {
+  /**
+   * Maps to CSS's **`visibility`** property
+   * @category Property
+   * @formalSyntaxForValue visible | hidden | collapse | inherit
+   * @added 0.2.0
+   * @implentationReference https://www.w3.org/TR/2011/REC-CSS2-20110607/visufx.html#propdef-visibility
+   */
   visibility: 'visible' | 'hidden' | 'collapse' | GlobalCssKeyword
 }
 
