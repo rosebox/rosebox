3ec9b226c9205ccecceb65ca8cb803f1f0084076
Hugo <hugonteifeh@gmail.com>
2020-06-19 09:33:12 +0200

    Bump up the version

--- src/core/align-content/index.test.ts
+++ src/core/align-content/index.test.ts
@@ -1,5 +1,15 @@
 import { style } from '../../index'
 
+test('alignContent: center', () => {
+  const received = style({
+    alignContent: 'center',
+  })
+  const expected = {
+    alignContent: 'center',
+  }
+  expect(received).toEqual(expected)
+})
+
 test('alignContent: ["first", "baseline"]', () => {
   const received = style({
     alignContent: ['first', 'baseline'],
