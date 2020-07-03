b5722e29af6a843a36aefe2841f24dcb0f5c2ee4
Hugo <hugonteifeh@gmail.com>
2020-07-03 12:54:26 +0200

    Update backgroundPosition

--- src/core/background-position/index.test.ts
+++ src/core/background-position/index.test.ts
@@ -1,22 +1,20 @@
 import { style } from '../../index'
 import { per } from '../shared/value-constructors'
+import { pos } from '.'
 
 test('backgroundPosition: [center, bottom]', () => {
   const received = style({
-    backgroundPosition: ['center', 'bottom'],
+    backgroundPosition: pos(per(50), per(50)),
   })
   const expected = {
-    backgroundPosition: 'center bottom',
+    backgroundPosition: '50% 50%',
   }
   expect(received).toEqual(expected)
 })
 
 test('backgroundPosition: [[left, 60%], [bottom, 30%]]', () => {
   const received = style({
-    backgroundPosition: [
-      ['left', per(60)],
-      ['bottom', per(30)],
-    ],
+    backgroundPosition: pos(['left', per(60)], ['bottom', per(30)]),
   })
   const expected = {
     backgroundPosition: 'left 60% bottom 30%',
@@ -26,10 +24,20 @@ test('backgroundPosition: [[left, 60%], [bottom, 30%]]', () => {
 
 test('backgroundPosition: [center, [bottom, 30%]]', () => {
   const received = style({
-    backgroundPosition: ['center', ['bottom', per(30)]],
+    backgroundPosition: pos('center', ['bottom', per(30)]),
   })
   const expected = {
     backgroundPosition: 'center bottom 30%',
+  }
+  expect(received).toEqual(expected)
+})
+
+test('backgroundPosition: [pos(50%, 50%), pos(-40%, 30%)', () => {
+  const received = style({
+    backgroundPosition: [pos(per(50), per(50)), pos(per(-40), per(30))],
+  })
+  const expected = {
+    backgroundPosition: '50% 50%, -40% 30%',
   }
   expect(received).toEqual(expected)
 })
--- src/core/background-position/index.ts
+++ src/core/background-position/index.ts
@@ -1,10 +1,8 @@
-import { serializeColor, Color } from '../color'
 import {
-  GlobalCssKeyword,
-  isGlobalCssKeyword,
   LengthPercentage,
   isLengthType,
   isPercentageType,
+  GlobalCssKeyword,
 } from '../shared/types'
 import {
   serializeLength,
@@ -12,45 +10,34 @@ import {
   serializeLengthPercentage,
 } from '../shared/serializers'
 
-export type BackgroundColorCSSProp = 'background-color'
-
-export const serializeBackgroundColorValue = (
-  value: Color | GlobalCssKeyword
-): string => (isGlobalCssKeyword(value) ? value : serializeColor(value))
+export const serializeAtomicValue = (x: Position): string => {
+  if (typeof x.value === 'string') return x.value
+  const pos = x.value
+  return pos.reduce((acc, val, idx) => {
+    const serializedVal =
+      typeof val === 'string'
+        ? val
+        : isLengthType(val)
+        ? acc + serializeLength(val)
+        : isPercentageType(val)
+        ? serializePercentage(val)
+        : `${val[0]} ${serializeLengthPercentage(val[1])}`
+    return acc + serializedVal + (idx === pos.length - 1 ? '' : ' ')
+  }, '')
+}
 
 export const serializeBgPositionPropValue = (
-  x:
-    | [
-        'left' | 'center' | 'right' | LengthPercentage,
-        'top' | 'center' | 'bottom' | LengthPercentage
-      ]
-    | [
-        'center' | ['left' | 'right', LengthPercentage],
-        'center' | ['top' | 'bottom', LengthPercentage]
-      ]
-    | GlobalCssKeyword
-): string =>
-  typeof x === 'string'
-    ? x
-    : (x as (
-        | 'left'
-        | 'right'
-        | 'center'
-        | 'bottom'
-        | 'top'
-        | LengthPercentage
-        | [string, LengthPercentage]
-      )[]).reduce((acc, val, idx) => {
-        const serializedVal =
-          typeof val === 'string'
-            ? val
-            : isLengthType(val)
-            ? acc + serializeLength(val)
-            : isPercentageType(val)
-            ? serializePercentage(val)
-            : `${val[0]} ${serializeLengthPercentage(val[1])}`
-        return acc + serializedVal + (idx === x.length - 1 ? '' : ' ')
-      }, '')
+  x: Position | Position[]
+): string => {
+  if (typeof x === 'string') return x
+  if (Array.isArray(x))
+    return x.reduce(
+      (acc, val, idx) =>
+        acc + serializeAtomicValue(val) + (idx === x.length - 1 ? '' : ', '),
+      ''
+    )
+  return serializeAtomicValue(x)
+}
 
 /**
  * @category RBDeclarationTypeAlias
@@ -59,20 +46,61 @@ export type BgPositionDeclaration = {
   /**
    * A RB property that maps to CSS's **`background-position`** property
    * @category RBProperty
-   * @formalSyntaxForValue [
-   * [ left | center | right | <length-percentage> ] [ top | center | bottom | <length-percentage> ]
-   * | [ center | [ left | right ] <length-percentage>? ] & [ center | [ top | bottom ] <length-percentage>? ]
 ]
    * @added https://www.w3.org/TR/2011/REC-CSS2-20110607/colors.html#propdef-background-image
    */
-  backgroundPosition:
-    | [
-        'left' | 'center' | 'right' | LengthPercentage,
-        'top' | 'center' | 'bottom' | LengthPercentage
-      ]
-    | [
-        'center' | ['left' | 'right', LengthPercentage],
-        'center' | ['top' | 'bottom', LengthPercentage]
-      ]
-    | GlobalCssKeyword
+  backgroundPosition: Position | Position[] | GlobalCssKeyword
+}
+
+export interface Position {
+  __tag: 'Position'
+  value: [
+    (
+      | 'left'
+      | 'center'
+      | 'right'
+      | LengthPercentage
+      | 'center'
+      | ['left' | 'right', LengthPercentage]
+    ),
+    (
+      | 'top'
+      | 'center'
+      | 'bottom'
+      | LengthPercentage
+      | 'center'
+      | ['top' | 'bottom', LengthPercentage]
+    )
+  ]
+}
+
+export function pos(
+  x: 'left' | 'center' | 'right' | LengthPercentage,
+  y: 'top' | 'center' | 'bottom' | LengthPercentage
+): Position
+export function pos(
+  x: 'center' | ['left' | 'right', LengthPercentage],
+  y: 'center' | ['top' | 'bottom', LengthPercentage]
+): Position
+
+export function pos(
+  x:
+    | 'left'
+    | 'center'
+    | 'right'
+    | LengthPercentage
+    | 'center'
+    | ['left' | 'right', LengthPercentage],
+  y:
+    | 'top'
+    | 'center'
+    | 'bottom'
+    | LengthPercentage
+    | 'center'
+    | ['top' | 'bottom', LengthPercentage]
+): Position {
+  return {
+    __tag: 'Position',
+    value: [x, y],
+  }
 }
