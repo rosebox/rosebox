60289055c20df9f3acd890f548976b2f9076a796
Hugo <hugonteifeh@gmail.com>
2020-07-04 17:46:30 +0200

    Update display

--- src/core/display/index.ts
+++ src/core/display/index.ts
@@ -15,136 +15,104 @@ type DisplayInsideOutsideDoubleBar =
   | 'flex'
   | 'grid'
   | 'ruby'
-  | 'block flow'
-  | 'block flow-root'
-  | 'block table'
-  | 'block flex'
-  | 'block grid'
-  | 'block ruby'
-  | 'flow block'
-  | 'flow-root block'
-  | 'table block'
-  | 'flex block'
-  | 'grid block'
-  | 'ruby block'
-  | 'inline flow'
-  | 'inline flow-root'
-  | 'inline table'
-  | 'inline flex'
-  | 'inline grid'
-  | 'inline ruby'
-  | 'flow inline'
-  | 'flow-root inline'
-  | 'table inline'
-  | 'flex inline'
-  | 'grid inline'
-  | 'ruby inline'
-  | 'run-in flow'
-  | 'run-in flow-root'
-  | 'run-in table'
-  | 'run-in flex'
-  | 'run-in grid'
-  | 'run-in ruby'
-  | 'flow run-in'
-  | 'flow-root run-in'
-  | 'table run-in'
-  | 'flex run-in'
-  | 'grid run-in'
-  | 'ruby run-in'
-
-/**
- * @experimental
- */
-export const disBlock = { display: 'block' } as const
-/**
- * @experimental
- */
-export const disInline = { display: 'inline' } as const
-/**
- * @experimental
- */
-export const disRunIn = { display: 'run-in' } as const
-/**
- * @experimental
- */
-export const disFlow = { display: 'flow' } as const
-/**
- * @experimental
- */
-export const disFlowRoot = { display: 'flow-root' } as const
-/**
- * @experimental
- */
-export const disTable = { display: 'table' } as const
-/**
- * @experimental
- */
-export const disFlex = { display: 'flex' } as const
-/**
- * @experimental
- */
-export const disGrid = { display: 'grid' } as const
-/**
- * @experimental
- */
-export const disRuby = { display: 'ruby' } as const
+  | ['block']
+  | ['inline']
+  | ['run-in']
+  | ['flow']
+  | ['flow-root']
+  | ['table']
+  | ['flex']
+  | ['grid']
+  | ['ruby']
+  | ['block', 'flow']
+  | ['block', 'flow-root']
+  | ['block', 'table']
+  | ['block', 'flex']
+  | ['block', 'grid']
+  | ['block', 'ruby']
+  | ['flow', 'block']
+  | ['flow-root', 'block']
+  | ['table', 'block']
+  | ['flex', 'block']
+  | ['grid', 'block']
+  | ['ruby', 'block']
+  | ['inline', 'flow']
+  | ['inline', 'flow-root']
+  | ['inline', 'table']
+  | ['inline', 'flex']
+  | ['inline', 'grid']
+  | ['inline', 'ruby']
+  | ['flow', 'inline']
+  | ['flow-root', 'inline']
+  | ['table', 'inline']
+  | ['flex', 'inline']
+  | ['grid', 'inline']
+  | ['ruby', 'inline']
+  | ['run-in', 'flow']
+  | ['run-in', 'flow-root']
+  | ['run-in', 'table']
+  | ['run-in', 'flex']
+  | ['run-in', 'grid']
+  | ['run-in', 'ruby']
+  | ['flow', 'run-in']
+  | ['flow-root', 'run-in']
+  | ['table', 'run-in']
+  | ['flex', 'run-in']
+  | ['grid', 'run-in']
+  | ['ruby', 'run-in']
 
 /**
  * @skip
  */
 type DisplayListItem =
   | 'list-item'
-  | 'list-item flow'
-  | 'flow list-item'
-  | 'list-item flow-root'
-  | 'flow-root list-item'
-  | 'list-item block'
-  | 'block list-item'
-  | 'list-item inline'
-  | 'inline list-item'
-  | 'list-item run-in'
-  | 'run-ine list-item'
-  | 'list-item flow block'
-  | 'list-item block flow'
-  | 'block flow list-item'
-  | 'block list-item flow'
-  | 'flow list-item block'
-  | 'flow block list-item'
-  | 'list-item flow inline'
-  | 'list-item inline flow'
-  | 'inline flow list-item'
-  | 'inline list-item flow'
-  | 'flow inline list-item'
-  | 'flow list-item inline'
-  | 'list-item flow run-in'
-  | 'list-item run-in flow'
-  | 'run-in flow list-item'
-  | 'run-in list-item flow'
-  | 'flow run-in list-item'
-  | 'flow list-item run-in'
-  | 'list-item flow-root block'
-  | 'list-item block flow-root'
-  | 'block flow-root list-item'
-  | 'block list-item flow-root'
-  | 'flow-root block list-item'
-  | 'flow-root list-item block'
-  | 'list-item flow-root inline'
-  | 'list-item inline flow-root'
-  | 'inline flow-root list-item'
-  | 'inline list-item flow-root'
-  | 'flow-root inline list-item'
-  | 'flow-root list-item inline'
-  | 'list-item flow-root run-in'
-  | 'list-item run-in flow-root'
-  | 'run-in flow-root list-item'
-  | 'run-in list-item flow-root'
-  | 'flow-root run-in list-item'
-  | 'flow-root list-item run-in'
-
-/**
- * @experimental
- */
-export const disListItem = { display: 'list-item' } as const
+  | ['list-item']
+  | ['list-item', 'flow']
+  | ['flow', 'list-item']
+  | ['list-item', 'flow-root']
+  | ['flow-root', 'list-item']
+  | ['list-item', 'block']
+  | ['block', 'list-item']
+  | ['list-item', 'inline']
+  | ['inline', 'list-item']
+  | ['list-item', 'run-in']
+  | ['run-ine', 'list-item']
+  | ['list-item', 'flow', 'block']
+  | ['list-item', 'block', 'flow']
+  | ['block', 'flow', 'list-item']
+  | ['block', 'list-item', 'flow']
+  | ['flow', 'list-item', 'block']
+  | ['flow', 'block', 'list-item']
+  | ['list-item', 'flow', 'inline']
+  | ['list-item', 'inline', 'flow']
+  | ['inline', 'flow', 'list-item']
+  | ['inline', 'list-item', 'flow']
+  | ['flow', 'inline', 'list-item']
+  | ['flow', 'list-item', 'inline']
+  | ['list-item', 'flow', 'run-in']
+  | ['list-item', 'run-in', 'flow']
+  | ['run-in', 'flow', 'list-item']
+  | ['run-in', 'list-item', 'flow']
+  | ['flow', 'run-in', 'list-item']
+  | ['flow', 'list-item', 'run-in']
+  | ['list-item', 'flow-root', 'block']
+  | ['list-item', 'block', 'flow-root']
+  | ['block', 'flow-root', 'list-item']
+  | ['block', 'list-item', 'flow-root']
+  | ['flow-root', 'block', 'list-item']
+  | ['flow-root', 'list-item', 'block']
+  | ['list-item', 'flow-root', 'inline']
+  | ['list-item', 'inline', 'flow-root']
+  | ['inline', 'flow-root', 'list-item']
+  | ['inline', 'list-item', 'flow-root']
+  | ['flow-root', 'inline', 'list-item']
+  | ['flow-root', 'list-item', 'inline']
+  | ['list-item', 'flow-root', 'run-in']
+  | ['list-item', 'run-in', 'flow-root']
+  | ['run-in', 'flow-root', 'list-item']
+  | ['run-in', 'list-item', 'flow-root']
+  | ['flow-root', 'run-in', 'list-item']
+  | ['flow-root', 'list-item', 'run-in']
 
 /**
  * @skip
@@ -162,6 +130,18 @@ type DisplayInternal =
   | 'ruby-text'
   | 'ruby-base-container'
   | 'ruby-text-container'
+  | ['table-row-group']
+  | ['table-header-group']
+  | ['table-footer-group']
+  | ['table-row']
+  | ['table-cell']
+  | ['table-column-group']
+  | ['table-column']
+  | ['table-caption']
+  | ['ruby-base']
+  | ['ruby-text']
+  | ['ruby-base-container']
+  | ['ruby-text-container']
 
 /**
  * @skip
@@ -176,23 +156,10 @@ type DisplayLegacy =
   | 'inline-table'
   | 'inline-flex'
   | 'inline-grid'
-
-/**
- * @experimental
- */
-export const disInlineBlock = { display: 'inline-block' } as const
-/**
- * @experimental
- */
-export const disInlineTable = { display: 'inline-table' } as const
-/**
- * @experimental
- */
-export const disInlineFlex = { display: 'inline-flex' } as const
-/**
- * @experimental
- */
-export const disInlineGrid = { display: 'inline-grid' } as const
+  | ['inline-block']
+  | ['inline-table']
+  | ['inline-flex']
+  | ['inline-grid']
 
 /**
  * Formal syntax:
@@ -205,9 +172,16 @@ export type DisplayValue =
   | DisplayInternal
   | DisplayBox
 
-export const serializeDisplayValue = (value: DisplayValue | GlobalCssKeyword) =>
-  value
-
+export const serializeDisplayValue = (
+  x: DisplayValue | GlobalCssKeyword
+): string =>
+  Array.isArray(x)
+    ? (x as string[]).reduce(
+        (acc: string, val: string, idx: number) =>
+          `${acc}${val}${idx === x.length - 1 ? '' : ' '}`,
+        ''
+      )
+    : x
 /**
  * @category RBDeclarationTypeAlias
  */
