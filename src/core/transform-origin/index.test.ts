0d93187487c3dddfccec954959d7ad8ac754628a
Hugo <hugonteifeh@gmail.com>
2020-07-20 16:13:04 +0200

    Update tests for transformOrigin

--- src/core/transform-origin/index.test.ts
+++ src/core/transform-origin/index.test.ts
@@ -3,34 +3,24 @@ import { per, px } from '../shared'
 
 test('transormOrigin([keyword, keyword])', () => {
   const received = {
-    transformOrigin: serializeTransformOriginPropertyValue(['center', 'top']),
+    transformOrigin: serializeTransformOriginPropertyValue([per(50), px(0)]),
   }
   const expected = {
-    transformOrigin: 'center top',
+    transformOrigin: '50% 0px',
   }
   expect(received).toEqual(expected)
 })
 
-test('transormOrigin([keyword, LengthPercentage])', () => {
-  const received = {
-    transformOrigin: serializeTransformOriginPropertyValue(['center', px(100)]),
-  }
-  const expected = {
-    transformOrigin: 'center 100px',
-  }
-  expect(received).toEqual(expected)
-})
-
-test('transormOrigin([keyword, LengthPercentage, Length])', () => {
+test('transormOrigin([LengthPercentage, LengthPercentage, Length])', () => {
   const received = {
     transformOrigin: serializeTransformOriginPropertyValue([
-      'center',
+      per(100),
       per(100),
       px(50),
     ]),
   }
   const expected = {
-    transformOrigin: 'center 100% 50px',
+    transformOrigin: '100% 100% 50px',
   }
   expect(received).toEqual(expected)
 })
