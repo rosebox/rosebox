1e39ae6626a7da6e55ce8c2c439cda6e6bc48ccc
Hugo <hugonteifeh@gmail.com>
2020-07-25 16:15:39 +0200

    Refactor flexDirection

---
+++ src/core/flexbox/flex-direction/index.test.ts
@@ -0,0 +1,11 @@
+import { style } from '../../..'
+
+test('flexDirection', () => {
+  const received = style({
+    flexDirection: 'row',
+  })
+  const expected = {
+    flexDirection: 'row',
+  }
+  expect(received).toEqual(expected)
+})
