be5a962e03bdc0421a47248f2d2c2522daace5ba
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-08-13 17:42:58 +0200

    Feature/support color funcs (#26)
    * Add support for HSL
    
    * Add more tests
    
    * Add tagging
    
    * Update Color types

--- src/core/color/index.test.ts
+++ src/core/color/index.test.ts
@@ -1,5 +1,6 @@
-import { hex } from '.'
+import { darken, hex, hsl, lighten } from '.'
 import { RBStyle, style } from '../..'
+import { per } from '../shared'
 
 const styleObj: RBStyle = {
   color: hex('#000'),
@@ -9,6 +10,28 @@ test('color', () => {
   const received = style(styleObj)
   const expected = {
     color: '#000',
+  }
+  expect(received).toEqual(expected)
+})
+
+test('darken: HSL', () => {
+  const baseColor = hsl(100, per(50), per(50))
+  const received = style({
+    color: darken(baseColor, 0.5),
+  })
+  const expected = {
+    color: 'hsl(100, 50%, 25%)',
+  }
+  expect(received).toEqual(expected)
+})
+
+test('lighten: HSL', () => {
+  const baseColor = hsl(100, per(50), per(50))
+  const received = style({
+    color: lighten(baseColor, 0.5),
+  })
+  const expected = {
+    color: 'hsl(100, 50%, 75%)',
   }
   expect(received).toEqual(expected)
 })
