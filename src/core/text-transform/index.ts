1a3a2eb862fcc6e2dbf354ba296d92808cee39ce
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-08-23 11:17:40 +0200

    Update syntax for textTransform (#56)

--- src/core/text-transform/index.ts
+++ src/core/text-transform/index.ts
@@ -9,40 +9,46 @@ type OneValue =
   | 'full-size-kana '
 
 type TwoValues =
-  | 'capitalize full-width'
-  | 'full-width capitalize'
-  | 'uppercase full-width'
-  | 'full-width uppercase'
-  | 'lowercase full-width'
-  | 'full-width lowercase'
-  | 'capitalize full-size-kana'
-  | 'full-size-kana capitalize'
-  | 'uppercase full-size-kana'
-  | 'full-size-kana uppercase'
-  | 'lowercase full-size-kana'
-  | 'full-size-kana lowercase'
+  | ['capitalize', 'full-width']
+  | ['full-width', 'capitalize']
+  | ['uppercase', 'full-width']
+  | ['full-width', 'uppercase']
+  | ['lowercase', 'full-width']
+  | ['full-width', 'lowercase']
+  | ['capitalize', 'full-size-kana']
+  | ['full-size-kana', 'capitalize']
+  | ['uppercase', 'full-size-kana']
+  | ['full-size-kana', 'uppercase']
+  | ['lowercase', 'full-size-kana']
+  | ['full-size-kana', 'lowercase']
 
 type ThreeValues =
-  | 'capitalize full-width full-size-kana'
-  | 'capitalize full-size-kana full-width'
-  | 'full-size-kana capitalize full-width'
-  | 'full-size-kana full-width capitalize'
-  | 'full-width full-size-kana capitalize'
-  | 'full-width capitalize full-size-kana'
-  | 'uppercase full-width full-size-kana'
-  | 'uppercase full-size-kana full-width'
-  | 'full-size-kana uppercase full-width'
-  | 'full-size-kana full-width uppercase'
-  | 'full-width full-size-kana uppercase'
-  | 'full-width uppercase full-size-kana'
-  | 'lowercase full-width full-size-kana'
-  | 'lowercase full-size-kana full-width'
-  | 'full-size-kana lowercase full-width'
-  | 'full-size-kana full-width lowercase'
-  | 'full-width full-size-kana lowercase'
-  | 'full-width lowercase full-size-kana'
+  | ['capitalize', 'full-width', 'full-size-kana']
+  | ['capitalize', 'full-size-kana', 'full-width']
+  | ['full-size-kana', 'capitalize', 'full-width']
+  | ['full-size-kana', 'full-width', 'capitalize']
+  | ['full-width', 'full-size-kana', 'capitalize']
+  | ['full-width', 'capitalize', 'full-size-kana']
+  | ['uppercase', 'full-width', 'full-size-kana']
+  | ['uppercase', 'full-size-kana', 'full-width']
+  | ['full-size-kana', 'uppercase', 'full-width']
+  | ['full-size-kana', 'full-width', 'uppercase']
+  | ['full-width', 'full-size-kana', 'uppercase']
+  | ['full-width', 'uppercase', 'full-size-kana']
+  | ['lowercase', 'full-width', 'full-size-kana']
+  | ['lowercase', 'full-size-kana', 'full-width']
+  | ['full-size-kana', 'lowercase', 'full-width']
+  | ['full-size-kana', 'full-width', 'lowercase']
+  | ['full-width', 'full-size-kana', 'lowercase']
+  | ['full-width', 'lowercase', 'full-size-kana']
 
 type TextTransform = OneValue | TwoValues | ThreeValues
+
+export const serializeTextTransform = (value: TextTransform) => ({
+  textTransform: !Array.isArray(value)
+    ? value
+    : (value as string[]).reduce((acc, item) => acc + ' ' + item, '').trim(),
+})
 
 /**
  * @category RBDeclarationTypeAlias
