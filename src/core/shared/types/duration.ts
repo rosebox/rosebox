3530eee7081cc131caf1acd4592ee3d8b02e91a0
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-11-22 15:41:57 +0100

    Make Duration a more fine-grained type (#64)
    * Make Duration a more fine-grained typ
    
    * Bump up version
    
    * Update README

--- src/core/shared/types/duration.ts
+++ src/core/shared/types/duration.ts
@@ -3,7 +3,6 @@ import {
   getTypeName,
   getValConstructor,
   NAMESPACE,
-  RBType,
 } from './shared'
 
 /**
@@ -11,13 +10,23 @@ import {
  * A type that maps to CSS's **`<Duration>`** type.
  * @added 0.2.1
  */
-export interface Duration extends RBType<'Duration', number> {
-  toNum: (x: Duration) => number
+export interface Duration<A extends 'milliseconds' | 'seconds' | 'any' = any> {
+  [NAMESPACE]: {
+    unit: A extends 'milliseconds' ? 'milliseconds' 
+    : A extends 'seconds'
+    ? 'seconds'
+    : ('seconds' | 'milliseconds')
+    data: number
+    type: 'Duration'
+    valueConstructor: (x: number) => Duration<any>
+    serializer: (x: Duration<A>) => string
+  }
+  toNum: (x: Duration<any>) => number
 }
 
-const toNum = (x: Duration): number => x[NAMESPACE].data
+const toNum = (x: Duration<any>): number => x[NAMESPACE].data
 
-export const serializeDuration = (x: Duration): string => {
+export const serializeDuration = (x: Duration<any>): string => {
   const unit = getValConstructor(x) === s ? 's' : 'ms'
   return `${getData(x)}${unit}`
 }
@@ -27,8 +36,9 @@ export const serializeDuration = (x: Duration): string => {
  * @category Value constructor
  * @added 0.2.1
  */
-export const s = (x: number): Duration => ({
+export const s = (x: number): Duration<'seconds'> => ({
   [NAMESPACE]: {
+    unit: 'seconds',
     type: 'Duration',
     data: x,
     valueConstructor: s,
@@ -42,15 +52,24 @@ export const s = (x: number): Duration => ({
  * @category Value constructor
  * @added 0.2.1
  */
-export const ms = (x: number): Duration => ({
+export const ms = (x: number): Duration<'milliseconds'> => ({
   [NAMESPACE]: {
     type: 'Duration',
+    unit: 'milliseconds',
     data: x,
     valueConstructor: ms,
     serializer: serializeDuration,
   },
   toNum,
 })
+
+export const toSeconds = (x: Duration<'milliseconds'>): Duration<'seconds'> => s(
+  getData(x) / 1000
+)
+
+export const toMilliseconds = (x: Duration<'seconds'>): Duration<'milliseconds'> => ms(
+  getData(x) * 1000
+)
 
 export const isDuration = (x: any): x is Duration =>
   getTypeName(x) === 'Duration'
