5a69744bccce16dddfa1b3d352e9e492dcce13ab
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-11-28 15:08:55 +0100

    Update the "Length" signature of "sub"

--- src/core/shared/types/math.ts
+++ src/core/shared/types/math.ts
@@ -1,26 +1,27 @@
-import { Duration, isDuration, ms } from './duration'
-import { Length } from './length'
+import { Duration, DurationUnit, isDuration } from './duration'
+import { Length, LengthUnit } from './length'
 
 export interface Setoid<A> {
   eq(x: A, y: A): boolean
 }
 
-export function sub(x1: Duration, x2: Duration): Duration
+export function sub<A extends LengthUnit>(x1: Length<A>, x2: Length<A>): Length<A>
 export function sub(x1: any, x2: any): any {
-  if (isDuration(x1)) return x1.sub(x1, x2)
-  return null
+  return x1.sub(x1, x2)
 }
 
+
+export function add<A extends LengthUnit>(x1: Length<A>, x2: Length<A>): Length<A>
 export function add(x1: Duration, x2: Duration): Duration
 export function add(x1: any, x2: any): any {
-  if (isDuration(x1)) return x1.add(x1, x2)
+  return x1.add(x1, x2)
   return null
 }
 
-export function mult(x1: Duration, x2: number): Duration
-export function mult(x1: number, x2: Duration): Duration
-export function mult(x1: Length, x2: number): Duration
-export function mult(x1: number, x2: Length): Duration
+export function mult<A extends DurationUnit>(x1: Duration<A>, x2: number): Duration<A>
+export function mult<A extends DurationUnit>(x1: number, x2: Duration<A>): Duration<A>
+export function mult<A extends LengthUnit>(x1: Length<A>, x2: number): Length<A>
+export function mult<A extends LengthUnit>(x1: number, x2: Length<A>): Length<A>
 export function mult(x1: any, x2: any) {
   const func = x1?.mult ?? x2?.mult
   return func(x1, x2)
