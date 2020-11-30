8acec805b443d31ccdf6f66f61f217eca3a5fce0
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-11-30 23:03:43 +0100

    Update docs and Bump up the package version

--- src/core/shared/types/math.ts
+++ src/core/shared/types/math.ts
@@ -7,7 +7,7 @@ export interface Setoid<A> {
 }
 
 /**
- * @category utilityFunction
+ * @category Utility function
  */
 export function sub<A extends LengthUnit>(x1: Length<A>, x2: Length<A>): Length<A>
 export function sub(x1: any, x2: any): any {
@@ -16,7 +16,7 @@ export function sub(x1: any, x2: any): any {
 
 
 /**
- * @category utilityFunction
+ * @category Utility function
  */
 export function add<A extends LengthUnit>(x1: Length<A>, x2: Length<A>): Length<A>
 export function add(x1: Duration, x2: Duration): Duration
@@ -25,7 +25,7 @@ export function add(x1: any, x2: any): any {
 }
 
 /**
- * @category utilityFunction
+ * @category Utility function
  */
 export function mult<A extends DurationUnit>(x1: Duration<A>, x2: number): Duration<A>
 export function mult<A extends DurationUnit>(x1: number, x2: Duration<A>): Duration<A>
@@ -37,7 +37,7 @@ export function mult(x1: any, x2: any) {
 }
 
 /**
- * @category utilityFunction
+ * @category Utility function
  */
 export function div<A extends LengthUnit>(x1: Length<A>, x2: number): Length<A>
 export function div(x1: any, x2: any) {
@@ -46,7 +46,7 @@ export function div(x1: any, x2: any) {
 }
 
 /**
- * @category utilityFunction
+ * @category Utility function
  */
 export function eq<A extends LengthUnit>(x1: Length<A>, x2: Length<A>): boolean
 export function eq<A extends DurationUnit>(x1: Duration<A>, x2: Duration<A>): boolean
