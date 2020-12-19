d3aa6fb5de1bc053e8a105aad95a2e1dd33aad73
Hugo Nteifeh <hugonteifeh@gmail.com>
2020-12-19 10:23:23 +0100

    Add another test case for backgroundOrigin

--- packages/core/src/core/background-origin/index.test.ts
+++ packages/core/src/core/background-origin/index.test.ts
@@ -1,11 +1,21 @@
 import { style } from '../../index'
 
-test('backgroundImage: keyword', () => {
+test('backgroundOrigin: keyword', () => {
     const received = style({
         backgroundOrigin: 'border-box',
     })
     const expected = {
         backgroundOrigin: 'border-box',
+    }
+    expect(received).toEqual(expected)
+})
+
+test('backgroundOrigin: keyword[]', () => {
+    const received = style({
+        backgroundOrigin: ['content-box', 'padding-box'],
+    })
+    const expected = {
+        backgroundOrigin: 'content-box, padding-box',
     }
     expect(received).toEqual(expected)
 })
